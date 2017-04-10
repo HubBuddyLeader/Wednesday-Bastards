bee3c3d9645ca12138288ef8297d922819f52509
michael nader <michael nader>
2017-04-10 17:33:31 +1000

    add records(unfinished)

--- dp/Program.cs
+++ dp/Program.cs
@@ -17,6 +17,7 @@ namespace dp
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //DID IT COMMIT?
         }
     }
 }
