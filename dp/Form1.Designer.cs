e085201ee7aad2fb805d815de4d805114513c4e8
HubBuddyLeader <100586937@student.swin.edu.au>
2017-04-12 09:38:59 +1000

    addded UI elements

--- dp/Form1.Designer.cs
+++ dp/Form1.Designer.cs
@@ -28,16 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fm_Database));
             this.btnUpload = new System.Windows.Forms.Button();
             this.lblTitle = new System.Windows.Forms.Label();
             this.txtProduct = new System.Windows.Forms.TextBox();
             this.txtPrice = new System.Windows.Forms.TextBox();
             this.txtStock = new System.Windows.Forms.TextBox();
             this.grpInputData = new System.Windows.Forms.GroupBox();
-            this.lblProduct = new System.Windows.Forms.Label();
-            this.lblPrice = new System.Windows.Forms.Label();
             this.lblStock = new System.Windows.Forms.Label();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.lblProduct = new System.Windows.Forms.Label();
+            this.pb = new System.Windows.Forms.PictureBox();
             this.grpInputData.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
             this.SuspendLayout();
             // 
             // btnUpload
@@ -62,21 +65,21 @@
             // 
             // txtProduct
             // 
-            this.txtProduct.Location = new System.Drawing.Point(296, 88);
+            this.txtProduct.Location = new System.Drawing.Point(188, 91);
             this.txtProduct.Name = "txtProduct";
             this.txtProduct.Size = new System.Drawing.Size(278, 31);
             this.txtProduct.TabIndex = 2;
             // 
             // txtPrice
             // 
-            this.txtPrice.Location = new System.Drawing.Point(296, 187);
+            this.txtPrice.Location = new System.Drawing.Point(188, 190);
             this.txtPrice.Name = "txtPrice";
             this.txtPrice.Size = new System.Drawing.Size(278, 31);
             this.txtPrice.TabIndex = 3;
             // 
             // txtStock
             // 
-            this.txtStock.Location = new System.Drawing.Point(296, 293);
+            this.txtStock.Location = new System.Drawing.Point(188, 296);
             this.txtStock.Name = "txtStock";
             this.txtStock.Size = new System.Drawing.Size(278, 31);
             this.txtStock.TabIndex = 4;
@@ -90,40 +93,52 @@
             this.grpInputData.Controls.Add(this.txtStock);
             this.grpInputData.Controls.Add(this.txtPrice);
             this.grpInputData.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.grpInputData.Location = new System.Drawing.Point(187, 227);
+            this.grpInputData.Location = new System.Drawing.Point(301, 181);
             this.grpInputData.Name = "grpInputData";
-            this.grpInputData.Size = new System.Drawing.Size(651, 397);
+            this.grpInputData.Size = new System.Drawing.Size(550, 397);
             this.grpInputData.TabIndex = 5;
             this.grpInputData.TabStop = false;
             this.grpInputData.Text = "Input Data";
             // 
+            // lblStock
+            // 
+            this.lblStock.AutoSize = true;
+            this.lblStock.ForeColor = System.Drawing.Color.Aqua;
+            this.lblStock.Location = new System.Drawing.Point(61, 299);
+            this.lblStock.Name = "lblStock";
+            this.lblStock.Size = new System.Drawing.Size(72, 25);
+            this.lblStock.TabIndex = 7;
+            this.lblStock.Text = "Stock:";
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.ForeColor = System.Drawing.Color.Aqua;
+            this.lblPrice.Location = new System.Drawing.Point(61, 193);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(67, 25);
+            this.lblPrice.TabIndex = 6;
+            this.lblPrice.Text = "Price:";
+            // 
             // lblProduct
             // 
             this.lblProduct.AutoSize = true;
-            this.lblProduct.Location = new System.Drawing.Point(169, 91);
+            this.lblProduct.ForeColor = System.Drawing.Color.Aqua;
+            this.lblProduct.Location = new System.Drawing.Point(61, 94);
             this.lblProduct.Name = "lblProduct";
             this.lblProduct.Size = new System.Drawing.Size(92, 25);
             this.lblProduct.TabIndex = 5;
             this.lblProduct.Text = "Product:";
             this.lblProduct.Click += new System.EventHandler(this.label1_Click);
             // 
-            // lblPrice
+            // pb
             // 
-            this.lblPrice.AutoSize = true;
-            this.lblPrice.Location = new System.Drawing.Point(169, 190);
-            this.lblPrice.Name = "lblPrice";
-            this.lblPrice.Size = new System.Drawing.Size(67, 25);
-            this.lblPrice.TabIndex = 6;
-            this.lblPrice.Text = "Price:";
-            // 
-            // lblStock
-            // 
-            this.lblStock.AutoSize = true;
-            this.lblStock.Location = new System.Drawing.Point(169, 296);
-            this.lblStock.Name = "lblStock";
-            this.lblStock.Size = new System.Drawing.Size(72, 25);
-            this.lblStock.TabIndex = 7;
-            this.lblStock.Text = "Stock:";
+            this.pb.InitialImage = ((System.Drawing.Image)(resources.GetObject("pb.InitialImage")));
+            this.pb.Location = new System.Drawing.Point(123, 657);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(342, 239);
+            this.pb.TabIndex = 6;
+            this.pb.TabStop = false;
             // 
             // fm_Database
             // 
@@ -131,6 +146,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(1166, 983);
+            this.Controls.Add(this.pb);
             this.Controls.Add(this.grpInputData);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.btnUpload);
@@ -138,6 +154,7 @@
             this.Text = "PhP-SRePS";
             this.grpInputData.ResumeLayout(false);
             this.grpInputData.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -154,6 +171,7 @@
         private System.Windows.Forms.Label lblProduct;
         private System.Windows.Forms.Label lblStock;
         private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.PictureBox pb;
     }
 }
 
