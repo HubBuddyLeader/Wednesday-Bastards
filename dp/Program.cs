b0c7d3a0397089bd62c7d2e96409dac36181eb08
Nader7 <mnader767@gmail.com>
2017-04-19 11:01:12 +1000

    basic input code

--- dp/Program.cs
+++ dp/Program.cs
@@ -8,17 +8,19 @@ namespace dp
 {
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
+            List<Item> items = new List<Item>();
+            addForm addItem = new addForm();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //show me the magic
             Application.Run(new fm_Database());
             Application.Run(new fm_Database());
+            //magic showed
+
+
         }
     }
 }
