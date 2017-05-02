892795f76e5d19b75fc90aef0696900a8c1fadcc
HubBuddyLeader <100586937@student.swin.edu.au>
2017-05-02 13:23:56 +1000

    Minor code changes.  formatting

--- dp/EditForm.cs
+++ dp/EditForm.cs
@@ -34,11 +34,22 @@ namespace dp
 
         private void grpInputData_Enter(object sender, EventArgs e)
         {
-
+            // Do nothing..
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            selected = this.comboBox1.GetItemText(this.comboBox1.SelectedItem);
+            for (int i = 0; i < Database.Data.Count; i++)
+            {
+                if (Database.Data[i].Name == selected)
+                {
+                    index = i;
+                    txtProduct.Text = Database.Data[i].Name;
+                    //	txtStock.Text = Database.Data[i].Stock;
+                    //	txtPrice.Text = Database.Data[i].Price;
+                }
+            }
         }
 
         private void btnUpload_Click(object sender, EventArgs e)
@@ -51,7 +62,6 @@ namespace dp
 
         private void EditForm_Load_1(object sender, EventArgs e)
         {
-
             for (int i = 0; i < Database.Data.Count; i++)
             {
                 comboBox1.Items.Add(Database.Data[i].Name);
@@ -70,15 +80,12 @@ namespace dp
                     //	txtStock.Text = Database.Data[i].Stock;
                     //	txtPrice.Text = Database.Data[i].Price;
                 }
-
             }
-
-
         }
 
         private void lblProduct_Click(object sender, EventArgs e)
         {
-
+            // Do nothing...
         }
     }
 }
