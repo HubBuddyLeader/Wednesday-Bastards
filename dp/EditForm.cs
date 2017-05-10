0ff5e97fec81e845017f6ebe60fefb199c7895b9
amiraliseighali <100630342@student.swin.edu.au>
2017-05-10 10:00:38 +1000

    edit done

--- dp/EditForm.cs
+++ dp/EditForm.cs
@@ -10,79 +10,79 @@ using System.Windows.Forms;
 
 namespace dp
 {
-    public partial class EditForm : Form
-    {
+	public partial class EditForm : Form
+	{
 		string selected;
 		int index;
 		public EditForm()
 		{
 			InitializeComponent();
-        }
+		}
 
-        private void editRecordToolStripMenuItem_Click(object sender, EventArgs e)
-        {
+		private void editRecordToolStripMenuItem_Click(object sender, EventArgs e)
+		{
 			for (int i = 0; i < Database.Data.Count; i++)
 			{
 				comboBox1.Items.Add(Database.Data[i].Name);
 			}
-        }
+		}
 
-        private void lblBack_Click(object sender, EventArgs e)
-        {
+		private void lblBack_Click(object sender, EventArgs e)
+		{
 			this.Hide();
-        }
+		}
 
-        private void grpInputData_Enter(object sender, EventArgs e)
-        {
+		private void grpInputData_Enter(object sender, EventArgs e)
+		{
 
-        }
+		}
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-        }
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+		}
 
-        private void btnUpload_Click(object sender, EventArgs e)
-        {
-			
+		private void btnUpload_Click(object sender, EventArgs e)
+		{
+
 			Database.Data[index].Name = txtProduct.Text;
-					//	txtStock.Text = Database.Data[i].Stock;
-					//	txtPrice.Text = Database.Data[i].Price;
-				
+			Database.Data[index].Stock = Convert.ToInt32(txtStock.Text);
+			Database.Data[index].Price = Convert.ToInt32(txtPrice.Text);
 
-			
-            
-        }
 
-        private void EditForm_Load_1(object sender, EventArgs e)
-        {
-            
-            for (int i = 0; i < Database.Data.Count; i++)
-            {
-                comboBox1.Items.Add(Database.Data[i].Name);
-            }
-        }
 
-        private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
-        {
+
+		}
+
+		private void EditForm_Load_1(object sender, EventArgs e)
+		{
+
+			for (int i = 0; i < Database.Data.Count; i++)
+			{
+				comboBox1.Items.Add(Database.Data[i].Name);
+			}
+		}
+
+		private void comboBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+		{
 			selected = this.comboBox1.GetItemText(this.comboBox1.SelectedItem);
 			for (int i = 0; i < Database.Data.Count; i++)
 			{
-				if (Database.Data[i].Name == selected) 
+				if (Database.Data[i].Name == selected)
 				{
 					index = i;
 					txtProduct.Text = Database.Data[i].Name;
-				//	txtStock.Text = Database.Data[i].Stock;
-				//	txtPrice.Text = Database.Data[i].Price;
+					txtStock.Text = Database.Data[i].Stock.ToString();
+					txtPrice.Text = Database.Data[i].Price.ToString();
 				}
-				   
-			 }
+
+			}
 
 
-        }
+		}
 
-        private void lblProduct_Click(object sender, EventArgs e)
-        {
+		private void lblProduct_Click(object sender, EventArgs e)
+		{
 
-        }
-    }
+		}
+	}
 }
